7f4d725e77b0248eccb54b5a58d3e7451bc4403e
Heltec-Aaron-Lee <support@heltec.cn>
2021-08-18 11:32:05 +0800

    modified sx1276 default max payload size

--- src/sx1276-board.h
+++ src/sx1276-board.h
@@ -49,7 +49,7 @@
     { MODEM_FSK , REG_IMAGECAL           , 0x02 },\
     { MODEM_FSK , REG_DIOMAPPING1        , 0x00 },\
     { MODEM_FSK , REG_DIOMAPPING2        , 0x30 },\
-    { MODEM_LORA, REG_LR_PAYLOADMAXLENGTH, 0x40 },\
+    { MODEM_LORA, REG_LR_PAYLOADMAXLENGTH, 0xFF },\
 }                                                 \
 
 #define RF_MID_BAND_THRESH                          525000000
